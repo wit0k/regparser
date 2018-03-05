779397e9ef0504cd6e8e8218ab94cb92d219e51a
wlawacz <wlawacz@gmail.com>
2018-03-05 11:06:21 +0100

    0.6.0

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ import subprocess
 program_name = "regparser"
 required_python_ver = (3,6,0)
 current_python_version = sys.version_info
+proxy_ip = ""  # Update proxy IP address if necessary
 
-pip_proxy_str = '--proxy http://30.207.31.50:8080'
+pip_proxy_str = '--proxy ' + 'http://' + proxy_ip + ':8080'
 required_dependencies = [
     'install --upgrade python-dateutil',
     'install --upgrade enum34',
@@ -15,9 +16,15 @@ required_dependencies = [
     'install --upgrade https://github.com/williballenthin/python-registry/archive/master.zip'
 ]
 
-def pip_install(cmd, via_proxy=True):
+def pip_install(cmd, via_proxy=False):
 
+    print("Via Proxy:" % (via_proxy))
     if via_proxy:
+
+        if not proxy_ip:
+            print("Error: Edit the script, the param 'proxy_ip' is empty!")
+            exit(-1)
+
         cmd = "pip " + pip_proxy_str + " " + cmd
         print(" - Execute: pip %s" % (cmd))
         subprocess.call(cmd, shell=True)
@@ -26,7 +33,7 @@ def pip_install(cmd, via_proxy=True):
         print(" - Execute: pip %s" % (cmd))
         subprocess.call(cmd, shell=True)
 
-print("Installing %s..." % (program_name))
+print("Installing %s's dependencies..." % (program_name))
 print("Checking dependencies:")
 
 try:
