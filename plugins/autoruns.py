660f5f0e8bdbaa44eb6309266adcbb2674b718bd
wlawacz <wlawacz@gmail.com>
2019-10-15 11:15:14 +0200

    FIX 0.9.2: - Autoruns: LPs from Hexacorn Values:
    r"Microsoft\Windows NT\CurrentVersion\AeDebugProtected\ProtectedDebugger"  # http://www.hexacorn.com/blog/2019/10/11/beyond-good-ol-run-key-part-119/

--- plugins/autoruns.py
+++ plugins/autoruns.py
@@ -104,7 +104,8 @@ class autoruns(object):
         r"ControlSet001\Control\ContentIndex\Language\English_UK\DLLOverridePat",
         r"ControlSet002\Control\ContentIndex\Language\English_US\DLLOverridePat",
         r"ControlSet003\Control\ContentIndex\Language\Neutral\DLLOverridePath",
-        r'Microsoft\Windows\Windows Error Reporting\Hangs\Debugger'  # http://www.hexacorn.com/blog/2019/09/20/beyond-good-ol-run-key-part-116/
+        r'Microsoft\Windows\Windows Error Reporting\Hangs\Debugger',  # http://www.hexacorn.com/blog/2019/09/20/beyond-good-ol-run-key-part-116/
+        r"Microsoft\Windows NT\CurrentVersion\AeDebugProtected\ProtectedDebugger"  # http://www.hexacorn.com/blog/2019/10/11/beyond-good-ol-run-key-part-119/
     ]
 
     QUERY_KEY_LIST = [
